d9e96bb36135dcfdd2bc43ca266366d1fd97b3a9
Tim Morgan <tim@timmorgan.org>
2009-06-03 07:15:45 -0500

    Use leading underscores for some of our internals.
    Internal-use instance variables and methods are prefixed with double
    underscore (__) to clean up the namespace a bit.

--- lib/cascading_rubies.rb
+++ lib/cascading_rubies.rb
@@ -8,13 +8,13 @@ class CascadingRubies
   # list of tags taken from http://www.w3schools.com/tags/default.asp
   TAGS = %w(a abbr acronym address applet area b base basefont bdo big blockquote body br button caption center cite code col colgroup dd del dir div dfn dl dt em fieldset font form frame frameset h1toh6 head hr html i iframe img input ins isindex kbd label legend li link map menu meta noframes noscript object ol optgroup option p param pre q s samp script select small span strike strong style sub sup table tbody td textarea tfoot th thead title tr tt u ul var xmp)
 
-  attr_reader :selectors, :css, :context_name, :has_children
+  attr_reader :__selectors, :__css, :__context_name, :__has_children
 
   def initialize(context_name=nil, selectors=[], classed=false, &dsl_code)
-    @context_name = context_name
-    @css = ''
-    @selectors = selectors
-    @classed = classed # if true, method_missing calls are css class selectors -- not id selectors
+    @__context_name = context_name
+    @__css = ''
+    @__selectors = selectors
+    @__classed = classed # if true, method_missing calls are css class selectors -- not id selectors
     if dsl_code.nil?
       # do nothing
     elsif dsl_code.arity == 1
@@ -26,37 +26,37 @@ class CascadingRubies
   
   def s(selector, *args, &dsl_code)
     if args.any? and dsl_code.nil? # css rule
-      @css << "#{selector}: #{args.join(' ')}; "
+      @__css << "#{selector}: #{args.join(' ')}; "
     else
       if args.any? and dsl_code # pseudo-class selector
         selector_name = args.map do |arg|
-          [[@context_name, selector].compact.join(' '), ':' + arg.to_s].join('')
+          [[@__context_name, selector].compact.join(' '), ':' + arg.to_s].join('')
         end.join(', ')
       else # regular selector
-        selector_name = [@context_name, selector].compact.join(@classed ? '' : ' ')
+        selector_name = [@__context_name, selector].compact.join(@__classed ? '' : ' ')
       end
-      if @bare and not @bare.has_children # e.g. li; a { }
-        selector_name = "#{@bare.context_name}, #{selector_name}"
+      if @bare and not @bare.__has_children # e.g. li; a { }
+        selector_name = "#{@bare.__context_name}, #{selector_name}"
       end
       if dsl_code
         compiled = self.class.new(selector_name, &dsl_code)
-        @selectors << "#{selector_name} { #{compiled.css}}" unless compiled.css.empty?
-        @selectors += compiled.selectors
+        @__selectors << "#{selector_name} { #{compiled.__css}}" unless compiled.__css.empty?
+        @__selectors += compiled.__selectors
         @bare = nil
       else # e.g. div.something
-        @bare = self.class.new(selector_name, @selectors, true, &dsl_code)
+        @bare = self.class.new(selector_name, @__selectors, true, &dsl_code)
       end
     end
   end
   
   def method_missing(method_name, *args, &dsl_code)
-    @has_children = true
-    selector = dsl_code ? method_name_to_selector(method_name) : dashify(method_name)
+    @__has_children = true
+    selector = dsl_code ? __method_name_to_selector(method_name) : __dashify(method_name)
     s(selector, *args, &dsl_code)
   end
 
   def to_s
-    @selectors.join("\n")
+    @__selectors.join("\n")
   end
   
   def self.parse(path)
@@ -67,20 +67,20 @@ class CascadingRubies
   
   private
   
-    def method_name_to_selector(method_name)
+    def __method_name_to_selector(method_name)
       selector = method_name.to_s
       if selector.sub!(/^_/, '.')
-        dashify(selector)
+        __dashify(selector)
       elsif TAGS.include?(selector)
-        dashify(selector)
-      elsif @classed
-        '.' + dashify(selector)
+        __dashify(selector)
+      elsif @__classed
+        '.' + __dashify(selector)
       else
-        '#' + dashify(selector)
+        '#' + __dashify(selector)
       end
     end
     
-    def dashify(name)
+    def __dashify(name)
       name.to_s.gsub(/_/, '-').downcase
     end
 
